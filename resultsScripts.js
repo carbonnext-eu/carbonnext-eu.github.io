200e766655cdb6b64bd3994102deec917bba7f7f
carbonnext-eu <37296815+carbonnext-eu@users.noreply.github.com>
2018-08-24 17:21:15 +0200

    Update resultsScripts.js

--- resultsScripts.js
+++ resultsScripts.js
@@ -57,7 +57,7 @@ d3.csv("results-data.csv", function(experiments) {
     .legend(dc.legend().x(550).y(10));
     chart.xAxis().ticks(3, "s");
     chart.yAxis().ticks(3, "s");
-  console.log(chart);
+  chart.render();
   sourceChart
   			.width(192)
         .radius(80)
